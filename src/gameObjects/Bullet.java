dd47a1c111d2254c4033d4507d7ff2f472dd3119
CanasianX <jaallenusa@gmail.com>
2015-09-20 23:29:02 -0500

    Revert "bullet animation thingy"
    This reverts commit 822a229c28f982a5f5554a7615008fd4016780c9.

--- src/gameObjects/Bullet.java
+++ src/gameObjects/Bullet.java
@@ -1,10 +1,7 @@
 package gameObjects;
 
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.geom.AffineTransform;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.ImageIcon;
 
@@ -21,48 +18,21 @@ public class Bullet {
 	
 	private int speed;
 	
-	private Image[] images;
-	private int currentImage;
-	
-	private long timer;
-	private long switchImage;
-	
 	public Bullet(int x, int y, double angle){
 		this.x = x;
 		this.y = y;
 		this.angle = angle;
 		
-		speed = 30;
+		speed = 20;
 		
 		double rad = Math.toRadians(angle);
 		dx = (int) (Math.cos(rad)*speed);
 		dy = (int) (Math.sin(rad)*speed);
-		
-		images = new Image[2];
-		
-		for(int i = 1; i <= images.length; i++){
-			images[i - 1] = new ImageIcon("images/bullet" + i + ".png").getImage();
-		}
-		
-		currentImage = 0;
-		
-		timer = System.nanoTime();
-		switchImage = 10;
 	}
 	
 	public boolean update(){
 		x += dx;
 		y += dy;
-		
-		long elapsed = (System.nanoTime() - timer)/1000000;
-		
-		if(elapsed >= switchImage){
-			timer = System.nanoTime();
-			currentImage++;
-			if(currentImage >= images.length){
-				currentImage = 0;
-			}
-		}
 		
 		if(x < 0 || x > GamePanel.width - 30 || y < -10 || y > GamePanel.height - 55){
 			return true;
@@ -76,7 +46,7 @@ public class Bullet {
 		
 		trans.rotate(Math.toRadians(angle), x + 20, y + 20); 
 		g.setTransform(trans);
-		g.drawImage(images[currentImage], x, y, 40, 40, null);
+		g.drawImage(new ImageIcon("images/bullet.png").getImage(), x, y, 40, 40, null);
 		
 		g.setTransform(backup);
 	}
--- src/gameObjects/Player.java
+++ src/gameObjects/Player.java
@@ -56,7 +56,7 @@ public class Player {
 		
 		firing = false;
 		firingTimer = System.nanoTime();
-		firingDelay = 500;
+		firingDelay = 200;
 	}
 	
 	public void setUp(boolean b){up = b;}
